e23700127422e291f725b88b2ff3e9fac74d1ff6
BlendAndroid <1059152860@qq.com>
2020-09-09 21:04:00 +0800

    Navigation

--- app/src/main/java/com/blend/jetpackstudy/MainActivity.java
+++ app/src/main/java/com/blend/jetpackstudy/MainActivity.java
@@ -1,19 +1,40 @@
 package com.blend.jetpackstudy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.blend.jetpackstudy.lifecycle.MyLocationListener;
+import com.blend.jetpackstudy.lifecycle.LifeCycleMainActivity;
+import com.blend.jetpackstudy.navigation.NavigationMainActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private Button lifecycle;
+    private Button navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lifecycle = findViewById(R.id.lifecycle);
+        navigation = findViewById(R.id.navigation);
+        lifecycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LifeCycleMainActivity.class));
+            }
+        });
+        navigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NavigationMainActivity.class));
+            }
+        });
 
     }
 }
