8eddec8327a00a8a1e7d61cf0b8116e8493adbc1
BlendAndroid <1059152860@qq.com>
2021-01-21 20:05:57 +0800

    ObservableField<LoginModel>

--- app/src/main/java/com/blend/jetpackstudy/databinding/TwoWayBindingViewModel2.java
+++ app/src/main/java/com/blend/jetpackstudy/databinding/TwoWayBindingViewModel2.java
@@ -2,42 +2,42 @@ package com.blend.jetpackstudy.databinding;
 
 import androidx.databinding.ObservableField;
 
-// public class TwoWayBindingViewModel2 {
-//
-//     private ObservableField<LoginModel> mLoginModelObservableField;
-//
-//     public TwoWayBindingViewModel2() {
-//         LoginModel loginModel = new LoginModel();
-//         loginModel.userName = "Blend";
-//         mLoginModelObservableField = new ObservableField<>();
-//         mLoginModelObservableField.set(loginModel);
-//     }
-//
-//     public String getUserName() {
-//         return mLoginModelObservableField.get().userName;
-//     }
-//
-//     public void setUserName(String userName) {
-//         mLoginModelObservableField.get().userName = userName;
-//         mLoginModelObservableField.set(mLoginModelObservableField.get());
-//         mLoginModelObservableField.notifyChange();
-//     }
-// }
-
 public class TwoWayBindingViewModel2 {
 
-    private ObservableField<String> mLoginModelObservableField;
+    private ObservableField<LoginModel> mLoginModelObservableField;
 
     public TwoWayBindingViewModel2() {
+        LoginModel loginModel = new LoginModel();
+        loginModel.userName = "Blend";
         mLoginModelObservableField = new ObservableField<>();
-        mLoginModelObservableField.set("Blend");
+        mLoginModelObservableField.set(loginModel);
     }
 
-    public ObservableField<String> getUserName() {
+    public ObservableField<LoginModel> getUserName() {
         return mLoginModelObservableField;
     }
 
     public void setUserName(String userName) {
-        mLoginModelObservableField.set(userName);
+        mLoginModelObservableField.get().userName = userName;
+        mLoginModelObservableField.set(mLoginModelObservableField.get());
+        mLoginModelObservableField.notifyChange();
     }
 }
+//
+// public class TwoWayBindingViewModel2 {
+//
+//     private ObservableField<String> mLoginModelObservableField;
+//
+//     public TwoWayBindingViewModel2() {
+//         mLoginModelObservableField = new ObservableField<>();
+//         mLoginModelObservableField.set("Blend");
+//     }
+//
+//     public ObservableField<String> getUserName() {
+//         return mLoginModelObservableField;
+//     }
+//
+//     public void setUserName(String userName) {
+//         mLoginModelObservableField.set(userName);
+//     }
+// }
