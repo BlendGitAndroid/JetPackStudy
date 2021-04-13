93bd0c201ea01ed373add8e7025b72c589036269
BlendAndroid <1059152860@qq.com>
2021-04-13 12:15:24 +0800

    深入了解Lifecycle

---
+++ app/src/main/java/com/blend/jetpackstudy/lifecycle/MyLifecycleOwner.java
@@ -0,0 +1,61 @@
+package com.blend.jetpackstudy.lifecycle;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LifecycleRegistry;
+import androidx.lifecycle.OnLifecycleEvent;
+
+public class MyLifecycleOwner implements LifecycleOwner {
+
+    private LifecycleRegistry mLifecycleRegistry;
+
+    public MyLifecycleOwner() {
+        //将LifecycleOwner作为LifecycleRegistry的弱引用
+        mLifecycleRegistry = new LifecycleRegistry(this);
+        mLifecycleRegistry.setCurrentState(Lifecycle.State.CREATED);
+    }
+
+
+    public void onStart() {
+        mLifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
+        getLifecycle().addObserver(new LifecycleObserver() {
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            public void onCreate() {
+
+            }
+        });
+
+        //其实所有的Observer最后都会被包装成LifecycleEventObserver
+        getLifecycle().addObserver(new LifecycleEventObserver() {
+            @Override
+            public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+
+            }
+        });
+
+        //java8之后应该这样使用
+        getLifecycle().addObserver(new DefaultLifecycleObserver() {
+            @Override
+            public void onStart(@NonNull LifecycleOwner owner) {
+
+            }
+        });
+
+    }
+
+    public void onStop() {
+        mLifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
+    }
+
+
+    @NonNull
+    @Override
+    public LifecycleRegistry getLifecycle() {
+        return mLifecycleRegistry;
+    }
+}
--- app/src/main/java/com/blend/jetpackstudy/lifecycle/MyLocationListener.java
+++ app/src/main/java/com/blend/jetpackstudy/lifecycle/MyLocationListener.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.OnLifecycleEvent;
 
 //通过注解的形式，进行生命周期的添加
@@ -16,14 +17,25 @@ public class MyLocationListener implements LifecycleObserver {
         Log.e(TAG, "MyLocationListener: ");
     }
 
+    //可以接受一个参数LifecycleOwner
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    private void startGetLocation() {
+    private void startGetLocation(LifecycleOwner owner) {
         Log.e(TAG, "startGetLocation: ");
+        //拥有者的生命周期至少是started的时候，才开始执行下面的代码
+        if (owner.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+
+        }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     private void stopGetLocation() {
         Log.e(TAG, "stopGetLocation: ");
+    }
+
+    //ANY注解，可以接受两个参数，第二个参数用于区分是什么事件
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    private void onAny(LifecycleOwner owner, Lifecycle.Event event) {
+
     }
 
 
