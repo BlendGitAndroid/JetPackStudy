1a577b4d3b32205a9e8c6c02287906df4ded0ba0
BlendAndroid <1059152860@qq.com>
2024-02-07 12:12:48 +0800

    Revert "Project"
    This reverts commit 787cf7d0ffc35fcfe8967bd9b6e511d156ef66ec.

--- app/src/main/java/com/blend/jetpackstudy/MainActivity.java
+++ app/src/main/java/com/blend/jetpackstudy/MainActivity.java
@@ -3,8 +3,10 @@ package com.blend.jetpackstudy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import com.blend.jetpackstudy.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.blend.jetpackstudy.databinding.DatabindingMainActivity;
 import com.blend.jetpackstudy.flow.FlowMainActivity;
 import com.blend.jetpackstudy.hit.HitMainActivity;
@@ -12,86 +14,109 @@ import com.blend.jetpackstudy.lifecycle.LifeCycleMainActivity;
 import com.blend.jetpackstudy.livedata.LiveDataMainActivity;
 import com.blend.jetpackstudy.navigation.NavigationMainActivity;
 import com.blend.jetpackstudy.page3.Page3MainActivity;
-import com.blend.jetpackstudy.project.ProjectMainActivity;
 import com.blend.jetpackstudy.room.RoomMainActivity;
-import com.blend.jetpackstudy.viewbinding.BaseActivity;
 import com.blend.jetpackstudy.viewbinding.ViewBindingActivity;
 import com.blend.jetpackstudy.viewmodel.ViewModelMainActivity;
+import com.blend.jetpackstudy.workmanager.WorkManagerMainActivity;
 
 
-public class MainActivity extends BaseActivity<ActivityMainBinding> {
+public class MainActivity extends AppCompatActivity {
 
+
+    private Button lifecycle;
+    private Button navigation;
+    private Button viewModel;
+    private Button liveData;
+    private Button room;
+    private Button workManager;
+    private Button dataBinding;
+    private Button viewBinding;
+    private Button hit;
+    private Button flow;
+    private Button page3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mViewBinding.lifecycle.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        lifecycle = findViewById(R.id.lifecycle);
+        navigation = findViewById(R.id.navigation);
+        viewModel = findViewById(R.id.viewModel);
+        liveData = findViewById(R.id.liveData);
+        room = findViewById(R.id.room);
+        workManager = findViewById(R.id.workManager);
+        dataBinding = findViewById(R.id.dataBinding);
+        viewBinding = findViewById(R.id.viewBinding);
+        hit = findViewById(R.id.hit);
+        flow = findViewById(R.id.flow);
+        page3 = findViewById(R.id.page3);
+        lifecycle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LifeCycleMainActivity.class));
             }
         });
-        mViewBinding.navigation.setOnClickListener(new View.OnClickListener() {
+        navigation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, NavigationMainActivity.class));
             }
         });
-        mViewBinding.viewModel.setOnClickListener(new View.OnClickListener() {
+        viewModel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ViewModelMainActivity.class));
             }
         });
-        mViewBinding.liveData.setOnClickListener(new View.OnClickListener() {
+        liveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LiveDataMainActivity.class));
             }
         });
-        mViewBinding.room.setOnClickListener(new View.OnClickListener() {
+        room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RoomMainActivity.class));
             }
         });
-        mViewBinding.dataBinding.setOnClickListener(new View.OnClickListener() {
+        workManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WorkManagerMainActivity.class));
+            }
+        });
+        dataBinding.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, DatabindingMainActivity.class));
             }
         });
-        mViewBinding.viewBinding.setOnClickListener(new View.OnClickListener() {
+        viewBinding.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ViewBindingActivity.class));
             }
         });
 
-        mViewBinding.hit.setOnClickListener(new View.OnClickListener() {
+        hit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, HitMainActivity.class));
             }
         });
 
-        mViewBinding.flow.setOnClickListener(new View.OnClickListener() {
+        flow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, FlowMainActivity.class));
             }
         });
 
-        mViewBinding.page3.setOnClickListener(new View.OnClickListener() {
+        page3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Page3MainActivity.class));
-            }
-        });
-        mViewBinding.project.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ProjectMainActivity.class));
             }
         });
     }
